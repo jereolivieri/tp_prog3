ac499cb602aaa61c4fb6065ef67fdd16bb275c84
jereolivieri <jeremiasolivieri@hotmail.com>
2024-04-28 17:14:26 -0300

    'Terminé' gbfs. No sé por qué no muestra el camino al llegar al final del laberinto. El ucs es el que me pasó Ceci

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -3,6 +3,12 @@ from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
+def manhattan(nodo_estado,final):
+    xf , yf = final 
+    x , y = nodo_estado
+    distancia = abs(x-xf)+abs(yf-y)
+    return distancia
+
 
 class GreedyBestFirstSearch:
     @staticmethod
@@ -19,35 +25,40 @@ class GreedyBestFirstSearch:
         node = Node("", grid.start, 0)
 
         frontier = PriorityQueueFrontier()
-        frontier.add(node)    
+
+        frontier.add(node,manhattan(node.state,grid.end))   
 
         # Initialize the explored dictionary to be empty
         explored = {node.state : node.cost} 
         
         while True:
-            if frontier.is_empty:
+
+            #print('después de agregar \n',frontier)
+
+            if frontier.is_empty():
+                #print('a \n')
                 return NoSolution(explored)
 
-            new_node = frontier.remove()
+            new_node = frontier.pop()
 
             if new_node.state==grid.end:
                 return Solution(new_node,explored)
-            
+
             successors=grid.get_neighbours(new_node.state)
-
+            #print('suc')
             for act in successors:
+                #print(act)
                 estado_nuevo=successors[act]
-
-                costo_nuevo = new_node.cost + estado_nuevo.cost
+                
+                costo_nuevo = new_node.cost + grid.get_cost(estado_nuevo)
 
                 if estado_nuevo not in explored or costo_nuevo < explored[estado_nuevo]:
                     new_node = Node("", estado_nuevo,
-                                    node.cost + grid.get_cost(estado_nuevo),
+                                    costo_nuevo,
                                     parent=node,action=act)
+                    
                     explored[estado_nuevo]=costo_nuevo
-                    frontier.add(new_node)
-                
 
-
+                    frontier.add(new_node,manhattan(new_node.state,grid.end))
 
         return NoSolution(explored)
--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -2,7 +2,7 @@ from ..models.grid import Grid
 from ..models.frontier import StackFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
-
+from ..models.frontier import PriorityQueueFrontier
 
 class UniformCostSearch:
     @staticmethod
@@ -24,4 +24,38 @@ class UniformCostSearch:
         # Add the node to the explored dictionary
         explored[node.state] = True
         
+        if node.state == grid.end:
+            return Solution(node,explored)
+
+        frontier = PriorityQueueFrontier()
+        frontier.add(node, node.cost)
+        explored = {node.state: node.cost}
+
+
+        while True:
+
+            if frontier.is_empty():
+                return NoSolution(explored)
+
+            node = frontier.pop()
+
+            successors=grid.get_neighbours(node.state)
+            
+            for act in successors:
+                estado_nuevo=successors[act]
+                
+                costo_nuevo = node.cost + estado_nuevo.cost
+
+                if estado_nuevo not in explored or costo_nuevo < explored[estado_nuevo].cost:
+                    new_node = Node("", estado_nuevo,
+                                    node.cost + grid.get_cost(estado_nuevo),
+                                    parent=node,action=act)
+
+                    if estado_nuevo == grid.end:
+                        return Solution(new_node, explored)                    
+
+                    explored[estado_nuevo]=True
+
+                    frontier.add(new_node.cost, new_node)
+
         return NoSolution(explored)
